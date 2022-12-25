5ea5609c43d692b3fd9aa750e507915c505ce41d
gdetosy <tosyjdm@gmail.com>
2022-12-25 09:26:38 +0300

    w

---
+++ Api/Api/UserViewController.swift
@@ -0,0 +1,31 @@
+//
+//  UserViewController.swift
+//  Api
+//
+//  Created by tosy on 25.12.22.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    @IBOutlet weak var email: UILabel!
+    
+    @IBOutlet weak var phone: UILabel!
+    
+    
+    @IBOutlet weak var website: UILabel!
+    
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var userNameLbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+    }  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  
+}
